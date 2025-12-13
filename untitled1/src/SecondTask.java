838f5594a847d1dc3f991a96435a8002c29b862b
User <ariksyc55@gmail.com>
2025-12-13 22:17:15 +0300

    Classes

--- untitled1/src/SecondTask.java
+++ untitled1/src/SecondTask.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class SecondTask {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Введите количество элементов массива");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Введите " + n + " чисел:");
+        for (i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
     }
+
 }
