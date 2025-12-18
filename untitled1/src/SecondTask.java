19c4946404122235473874ab3487bd6d5518f4cf
YaroslavSych <ariksyc55@gmail.com>
2025-12-18 14:33:53 +0300

    Task

--- untitled1/src/SecondTask.java
+++ untitled1/src/SecondTask.java
@@ -1,13 +1,32 @@
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class SecondTask {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите количество элементов массива");
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-        System.out.println("Введите " + n + " чисел:");
+        int[] numbers = {1, 3, 5, 7, 2, 4, 8, 6, 0, 9};
 
+        ArrayList<Integer> lessThanFive = new ArrayList<>();
+        ArrayList<Integer> fiveAndMore = new ArrayList<>();
+
+        for (int number : numbers) {
+            if (number < 5) {
+                lessThanFive.add(number);
+            } else {
+                fiveAndMore.add(number);
+            }
+        }
+
+        int[] arrayLessThanFive = lessThanFive.stream().mapToInt(i -> i).toArray();
+        int[] arrayFiveAndMore = fiveAndMore.stream().mapToInt(i -> i).toArray();
+
+        System.out.print("Числа меньше 5: ");
+        for (int num : arrayLessThanFive) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        System.out.print("Числа 5 и больше: ");
+        for (int num : arrayFiveAndMore) {
+            System.out.print(num + " ");
+        }
     }
-
 }
